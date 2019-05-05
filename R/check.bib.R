ad04409154212e296c48ec6d7e154bc710180058
Isaac Maddow-Zimet <imaddowzimet@gmail.com>
2019-05-05 15:55:39 -0400

    Initial commit

---
+++ R/check.bib.R
@@ -0,0 +1,34 @@
+#' Check Bibliography
+#'
+#' `check.bib` searches for all articles from a .bib file on Pubmed, and returns any associated retractions, erratums or expressions of concern.
+#'
+#' @param bibfile File path to .bib file
+#' @return Returns a list of warnings, if any articles in .bib file have any associated retractions, erratums or expressions of concern. If it does not, returns an empty list.
+#' @export
+#'
+#' @importFrom utils txtProgressBar
+
+
+
+check.bib <- function(bibfile) {
+  bibdata <- RefManageR::ReadBib(bibfile, check=F)
+  bibdataclean <- gsub("[{}]", "",bibdata$title)
+  timing <- length(bibdataclean)*1.5
+  print(paste("Warning: checking references now; expect a wait time of approximately", timing, "seconds"))
+  pg <- txtProgressBar(min = 0, max = length(bibdataclean), style = 3)
+  issues <- list()
+  num    <- 1
+  for(i in 1:length(bibdataclean)) {
+
+    article <- bibdataclean[i]
+    anynotice <- check.pubmed(article)
+    if (length(anynotice)>0) {
+
+      issues[num] <- anynotice
+      num <- num + 1
+    }
+    Sys.sleep(.5)
+    utils::setTxtProgressBar(pg, i)
+  }
+  return(issues)
+}
---
+++ R/check.pubmed.R
@@ -0,0 +1,39 @@
+#' Check Pubmed
+#'
+#' `check.pubmed` searches for a single article on Pubmed, and returns any associated retractions, erratums or expressions of concern.
+#'
+#' @param articlename Title of article (in quotes)
+#' @return Returns a list of warnings, if article has any associated retractions, erratums or expressions of concern. If it does not, returns an empty list.
+#' @examples
+#'
+#' check.pubmed("Ileal-lymphoid-nodular hyperplasia, non-specific colitis, and pervasive developmental disorder in children")
+#' # (returns warning that article may have been retracted)
+#'
+#' @export
+
+
+check.pubmed <- function(articlename) {
+
+  search_topic <- articlename
+  search_query <- RISmed::EUtilsSummary(search_topic, retmax=10)
+  records      <- RISmed::EUtilsGet(search_query)
+  pubmed_data  <- as.vector(RISmed::PublicationType(records))
+  retracted    <- grepl("Retraction of Publication", pubmed_data)
+  erratum      <- grepl("Published Erratum", pubmed_data)
+  concern      <- grepl("Expression of Concern", pubmed_data)
+  issues       <- list()
+  num          <- 1
+  if (sum(retracted) >= 1) {
+    issues[num] <- paste("Check: ", search_topic, ". It may have been retracted", sep="")
+    num <- num + 1
+  }
+  if (sum(erratum) >= 1) {
+    issues[num] <- paste("Check: ", search_topic, ". There may be an associated erratum", sep="")
+    num <- num + 1
+  }
+  if (sum(concern) >= 1) {
+    issues[num] <- paste("Check: ", search_topic, ". There may be an associated expression of concern", sep="")
+    num <- num + 1
+  }
+  return(issues)
+}

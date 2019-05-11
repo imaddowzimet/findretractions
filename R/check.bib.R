add60b05b618835fd2ae71ca924ddeaee693ac33
Isaac Maddow-Zimet <imaddowzimet@gmail.com>
2019-05-11 17:53:44 -0400

    check.bib now also checks crossref

--- R/check.bib.R
+++ R/check.bib.R
@@ -10,8 +10,15 @@
 
 
 
-check.bib <- function(bibfile) {
-  bibdata <- RefManageR::ReadBib(bibfile, check=F)
+check.bib <- function(bibfile, checkwith = "both") {
+
+  # Check for valid inputs
+  if (!(checkwith %in% c("both", "pubmed", "crossref"))) {
+    stop("Invalid input for checkwith: possible arguments are 'both', 'pubmed' or 'crossref'")
+  }
+
+  # Read BibTex file, extract titles, and loop over them
+  bibdata <- RefManageR::ReadBib(bibfile, check = F)
   bibdataclean <- gsub("[{}]", "",bibdata$title)
   timing <- length(bibdataclean)*1.5
   print(paste("Warning: checking references now; expect a wait time of approximately", timing, "seconds"))
@@ -19,27 +26,58 @@ check.bib <- function(bibfile) {
   issues <- list()
   num    <- 1
   for(i in 1:length(bibdataclean)) {
-
     article <- bibdataclean[i]
-    anynotice <- tryCatch(check.pubmed(article),
-                          warning = function(e) {
-                            closeAllConnections()
-                            Sys.sleep(1)
-                            check.pubmed(article)
-                          },
-                          error = function(e) {
-                            closeAllConnections()
-                            Sys.sleep(1)
-                            check.pubmed(article)
-                          }
-    )
-    if (length(anynotice)>0) {
+
+    # PubMed only
+    if (checkwith == "pubmed") {
+      anynotice <- tryCatch(check.pubmed(article),
+                            warning = function(e) {
+                              closeAllConnections()
+                              Sys.sleep(1)
+                              check.pubmed(article)
+                            },
+                            error = function(e) {
+                              closeAllConnections()
+                              Sys.sleep(1)
+                              check.pubmed(article)
+                            }
+                            )
+    }
+
+    # CrossRef only
+    if (checkwith == "crossref") {
+      anynotice <- check.crossref(article)
+    }
+
+    # Both
+    if (checkwith == "both") {
+      anynoticecrossref <- check.crossref(article)
+      anynoticepubmed   <- tryCatch(check.pubmed(article),
+                            warning = function(e) {
+                              closeAllConnections()
+                              Sys.sleep(1)
+                              check.pubmed(article)
+                            },
+                            error = function(e) {
+                              closeAllConnections()
+                              Sys.sleep(1)
+                              check.pubmed(article)
+                            }
+      )
+      if (length(anynoticecrossref) > 0) {anynotice <- anynoticecrossref}
+      if (length(anynoticepubmed) > 0)   {anynotice <- anynoticepubmed}
+      if (length(anynoticepubmed) == 0 & length(anynoticecrossref) == 0)   {anynotice <- list()}
+    }
+
+    # Store relevant results
+    if (length(anynotice) > 0) {
 
       issues[num] <- anynotice
       num <- num + 1
     }
     Sys.sleep(.5)
     utils::setTxtProgressBar(pg, i)
+
   }
   return(issues)
 }

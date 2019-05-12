3380982f82cee4ee80e58d2123d188c3471aba2f
Isaac Maddow-Zimet <imaddowzimet@gmail.com>
2019-05-12 19:23:02 -0400

    Added unit tests

---
+++ tests/testthat/test-check.R
@@ -0,0 +1,57 @@
+context("check.crossref")
+
+testthat::test_that("flags retraction", {
+  testthat::expect_equal(
+    check.crossref("Ileal-lymphoid-nodular hyperplasia, non-specific colitis, and pervasive developmental disorder in children"),
+    list("Check: Ileal-lymphoid-nodular hyperplasia, non-specific colitis, and pervasive developmental disorder in children. It may have been retracted")
+
+  )
+
+})
+
+testthat::test_that("does not flag retraction", {
+  testthat::expect_equal(
+    check.crossref("Changes in Adolescents' Receipt of Sex Education, 2006–2013"),
+    list()
+
+  )
+
+})
+
+context("check.pubmed")
+
+testthat::test_that("flags retraction", {
+  testthat::expect_equal(
+    check.pubmed("Ileal-lymphoid-nodular hyperplasia, non-specific colitis, and pervasive developmental disorder in children"),
+    list("Check: Ileal-lymphoid-nodular hyperplasia, non-specific colitis, and pervasive developmental disorder in children. It may have been retracted")
+
+  )
+
+})
+
+testthat::test_that("does not flag retraction", {
+  testthat::expect_equal(
+    check.pubmed("Changes in Adolescents' Receipt of Sex Education, 2006–2013"),
+    list()
+
+  )
+
+})
+
+
+
+
+context("check.bib")
+
+result <- list()
+result[[1]] <- "Check: Ileal-lymphoid-nodular hyperplasia, non-specific colitis, and pervasive developmental disorder in children. It may have been retracted"
+result[[2]] <- "Check: Genetic signatures of exceptional longevity in humans. It may have been retracted"
+result[[3]] <- "Check: Biological fabrication of cellulose fibers with tailored properties. There may be an associated erratum"
+result[[4]] <- "Check: Structural Stigma and All-Cause Mortality in Sexual Minority Populations. It may have been retracted"
+testthat::test_that("my_library test", {
+  testthat::expect_equal(
+    check.bib(here::here("misc", "My_Library.bib")),
+    result
+  )
+
+})

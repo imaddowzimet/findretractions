94436873ace74da080c5c923bdf134f96e076432
Isaac Maddow-Zimet <imaddowzimet@gmail.com>
2019-05-12 19:22:17 -0400

    Exempt articles with reatract or retraction in the title

--- R/check.crossref.R
+++ R/check.crossref.R
@@ -13,12 +13,16 @@
 
 
 check.crossref <- function(articlename) {
-
-  records <- rcrossref::cr_works(flq = c(`query.title` = articlename), select=c("title"))$data
-  matches  <- grep(articlename, dplyr::pull(records))
-  retracted    <- grepl("Retracted|Retraction", records[matches,])
   issues       <- list()
   num          <- 1
+  exempt   <- grepl("Retracted|Retraction", articlename, ignore.case = T)
+  if (exempt==T) {
+    warning(paste(articlename, "has the word 'retracted' or 'retraction' in the title, and will not be checked in CrossRef", sep=" "))
+    return(issues)
+  }
+  records <- rcrossref::cr_works(flq = c(`query.title` = articlename), select=c("title"))$data
+  matches  <- grep(articlename, dplyr::pull(records), ignore.case = T)
+  retracted    <- grepl("Retracted|Retraction", records[matches,], ignore.case = T)
   if (sum(retracted) >= 1) {
     issues[num] <- paste("Check: ", articlename, ". It may have been retracted", sep="")
     num <- num + 1
